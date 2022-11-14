444943f6f1d08c68db1097e3a4c2063a2597273f
Theron E. Weimer, Jr <wpafbo79@yahoo.com>
2022-11-13 19:29:34 -0500

    Add image for TencentARC GFPGAN.
    See: #11

---
+++ gfpgan/start.sh
@@ -0,0 +1,72 @@
+#!/bin/bash --login
+
+progname=$(basename $0)
+
+usage="
+Usage: ${progname} [-g|-h|-p|-u]
+  -g  Git log for most recent local commit.
+  -h  Show this usage statement.
+  -u  Update to most recent repository commit.
+"
+
+optGitLog=0
+optUpdate=0
+
+usage() { echo "$usage" 1>&2; exit 1; }
+
+while getopts ":ghpu" options; do
+  case "${options}" in
+    g)
+      optGitLog=1
+      ;;
+    h)
+      usage
+      ;;
+    u)
+      optUpdate=1
+      ;;
+    *)
+      usage
+      ;;
+  esac
+done
+shift $((OPTIND-1))
+
+if [ $optGitLog -eq 1 ]; then
+  echo -e "Most recent local Git commit for GFPGAN:\n"
+  git log --max-count=1
+
+  exit
+fi
+
+if [ $optUpdate -eq 1 ]; then
+  echo "Updating to most recent version of GFPGAN..."
+
+  # Fix issue with files already existing
+  export PIP_EXISTS_ACTION=w
+
+  git pull
+fi
+
+# Copy data from directories covered by volumes to populate volumes.
+for dir in $(ls -d archive/*); do
+  rsync -HaAxX --ignore-existing $dir/ $(basename $dir)/
+done
+
+if [ $(ls experiments/pretrained_models/*.pth | wc -l) -eq 0 ]; then
+  cat <<EOF
+No model found!
+Please download models as described on the project page.
+  https://github.com/TencentARC/GFPGAN
+
+Sleeping for 24 hours to allow time for the models to download.
+Restart container when the models are done downloading.
+EOF
+
+  sleep 24h
+
+  exit
+fi
+
+sleep 24h
+#echo "Running GFPGAN...  (This can take a few minutes.)"
