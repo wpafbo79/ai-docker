c472c8d8810a0c4536fd91551b94239e194a0cf9
Theron E. Weimer, Jr <wpafbo79@yahoo.com>
2022-12-08 02:10:59 -0500

    Rename mini-cuda to mini-cuda-base.
    Add a qualifier to the mini-cuda image since we want to add more
    packages to the base as an additional common image.
    
    Add updated compose file for the mini-cuda-base.
    
    See: #26

--- mini-cuda/config.sh
+++
@@ -1,9 +0,0 @@
-REPO=wpafbo79/mini-cuda
-
-CUDA_VERSION=$(grep "ENV CUDA_VERSION" Dockerfile | cut -d '=' -f 2)
-MINICONDA3_VERSION=$(grep "ENV MINICONDA3_VERSION" Dockerfile | cut -d '=' -f 2)
-TINI_VERSION=$(grep "ENV TINI_VERSION" Dockerfile | cut -d '=' -f 2)
-
-VERSION=cuda-${CUDA_VERSION}
-VERSION=${VERSION}_miniconda3-${MINICONDA3_VERSION}
-VERSION=${VERSION}_tini-${TINI_VERSION}
