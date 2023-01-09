235c51cf55eada29632e96ebce923dddafda0150
Theron E. Weimer, Jr <wpafbo79@yahoo.com>
2023-01-08 19:42:48 -0500

    Update InvokeAI to use GIT_COMMIT config variable.
    Also, update config volumes for the specific release.
    
    Rel: #34

--- invokeai/config.sh
+++ invokeai/config.sh
@@ -3,6 +3,7 @@ DEFAULT_NAME="invokeai"
 
 DOCKER_REPO="wpafbo79/invokeai"
 
+GIT_COMMIT="v2.2.5p2"
 GIT_REPO="https://github.com/invoke-ai/InvokeAI.git"
 
 INSTALL_DIR="/InvokeAI/"
@@ -11,12 +12,16 @@ ARCHIVE_DIR="${INSTALL_DIR}archive/"
 
 PORT_MAP="9090:9090"
 
-VERSION="v$(date -u +%Y%m%d-%H%M%S)"
+VERSION="${GIT_COMMIT}"
 
 declare -A VOLUMES
 # Don't use: b, h, n, v
 VOLUMES[c]="configs:${INSTALL_DIR}configs/"
+VOLUMES[e]="embeddings:${INSTALL_DIR}embeddings/"
+VOLUMES[f]="codeformer:${INSTALL_DIR}models/codeformer/"
+VOLUMES[g]="models-gfpgan:${INSTALL_DIR}models/gfpgan/"
 VOLUMES[l]="logs:${INSTALL_DIR}logs/"
-VOLUMES[m]="models:${INSTALL_DIR}models/"
 VOLUMES[o]="outputs:${INSTALL_DIR}outputs/"
+VOLUMES[r]="models-real-esrgan:${INSTALL_DIR}models/realesrgan/"
+VOLUMES[s]="models-stable-diffusion:${INSTALL_DIR}models/ldm/stable-diffusion-v1/"
 VOLUMES[t]="training-data:${INSTALL_DIR}training-data/"
