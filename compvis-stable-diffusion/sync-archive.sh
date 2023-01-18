886d4102f526817cce7dfbaa7f045a5f1813a09e
Theron E. Weimer, Jr <wpafbo79@yahoo.com>
2023-01-17 23:05:09 -0500

    Use the config file for the archive directory path.
    See: #39

--- compvis-stable-diffusion/sync-archive.sh
+++ compvis-stable-diffusion/sync-archive.sh
@@ -7,6 +7,6 @@ for vol in ${!VOLUMES[@]}; do
   dir=$(echo ${VOLUMES[${vol}]} | cut -d ":" -f 2)
   leaf=$(basename ${dir} /)
 
-  echo rsync -HaAxX --progress --ignore-existing archive/${leaf}/ ${dir}/
-  rsync -v -HaAxX --progress --ignore-existing archive/${leaf}/ ${dir}/
+  echo rsync -HaAxX --progress --ignore-existing ${ARCHIVE_DIR}/${leaf}/ ${dir}/
+  rsync -v -HaAxX --progress --ignore-existing ${ARCHIVE_DIR}/${leaf}/ ${dir}/
 done
