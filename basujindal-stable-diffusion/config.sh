d87851fc7588006f2ddc3a81eede4efeae41df4d
Theron E. Weimer, Jr <wpafbo79@yahoo.com>
2023-01-21 19:05:22 -0500

    Fix Basu Jindal's Stable diffusion version.
    Update Basu Jindal's Stable Diffusion to use GIT_COMMIT config variable.
    Use Gradio and setup exposed port.  Update config volumes for the
    specific release.
    
    Also, fix release to Cuda 11.8.0.
    
    See: #34

--- basujindal-stable-diffusion/config.sh
+++ basujindal-stable-diffusion/config.sh
@@ -3,20 +3,21 @@ DEFAULT_NAME="basujindal-stable-diffusion"
 
 DOCKER_REPO="wpafbo79/basujindal-stable-diffusion"
 
+GIT_COMMIT="54b4a91633e38f6060d29c4e0194efe8f66eeedc"
 GIT_REPO="https://github.com/basujindal/stable-diffusion.git"
 
 INSTALL_DIR="/stable-diffusion/"
 
 ARCHIVE_DIR="${INSTALL_DIR}archive/"
 
-PORT_MAP="none"
+PORT_MAP="7860:7860"
 
-VERSION="v$(date -u +%Y%m%d-%H%M%S)"
+VERSION="${GIT_COMMIT:0:7}"
 
 declare -A VOLUMES
 # Don't use: b, h, n, v
 VOLUMES[c]="configs:${INSTALL_DIR}configs/"
 VOLUMES[l]="logs:${INSTALL_DIR}logs/"
-VOLUMES[m]="models:${INSTALL_DIR}models/"
 VOLUMES[o]="outputs:${INSTALL_DIR}outputs/"
+VOLUMES[s]="models-stable-diffusion:${INSTALL_DIR}models/ldm/stable-diffusion-v1/"
 VOLUMES[t]="training-data:${INSTALL_DIR}training-data/"
