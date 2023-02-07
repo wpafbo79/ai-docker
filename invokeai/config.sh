8cbfe4292f21aff0afa34e3b779a75e67cc3254b
Theron E. Weimer, Jr <wpafbo79@yahoo.com>
2023-02-06 21:30:48 -0500

    Update Invoke-AI to v2.3.0.
    Follow new manual installation instructions.  Use commands setup by the
    virtual environment.
    
    Add additional volumes for new models and diffusers.
    
    See: #34

--- invokeai/config.sh
+++ invokeai/config.sh
@@ -1,9 +1,9 @@
 DEFAULT_CREATE_VERSION="latest"
 DEFAULT_NAME="invokeai"
 
-DOCKER_REPO="wpafbo79/invokeai"
+DOCKER_REPO="wpafbo79/${DEFAULT_NAME}"
 
-GIT_COMMIT="v2.2.5p2"
+GIT_COMMIT="v2.3.0"
 GIT_REPO="https://github.com/invoke-ai/InvokeAI.git"
 
 INSTALL_DIR="/InvokeAI/"
@@ -17,11 +17,13 @@ VERSION="${GIT_COMMIT}"
 declare -A VOLUMES
 # Don't use: b, h, n, v
 VOLUMES[c]="configs:${INSTALL_DIR}configs/"
+VOLUMES[d]="models-diffusers:${INSTALL_DIR}models/diffusers/"
 VOLUMES[e]="embeddings:${INSTALL_DIR}embeddings/"
-VOLUMES[f]="codeformer:${INSTALL_DIR}models/codeformer/"
+VOLUMES[f]="models-codeformer:${INSTALL_DIR}models/codeformer/"
 VOLUMES[g]="models-gfpgan:${INSTALL_DIR}models/gfpgan/"
 VOLUMES[l]="logs:${INSTALL_DIR}logs/"
 VOLUMES[o]="outputs:${INSTALL_DIR}outputs/"
 VOLUMES[r]="models-real-esrgan:${INSTALL_DIR}models/realesrgan/"
 VOLUMES[s]="models-stable-diffusion:${INSTALL_DIR}models/ldm/stable-diffusion-v1/"
 VOLUMES[t]="training-data:${INSTALL_DIR}training-data/"
+VOLUMES[u]="models-hub:${INSTALL_DIR}models/hub/"
