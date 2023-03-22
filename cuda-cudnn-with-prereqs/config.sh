3923ae3b1adbc273e2a764a209cf10539ab63df6
Theron E. Weimer, Jr <wpafbo79@yahoo.com>
2023-03-21 22:53:22 -0400

    Add image for Cuda with cuDNN.
    Create a base image and one with TensorRT.
    
    Rel: #77

---
+++ cuda-cudnn-with-prereqs/config.sh
@@ -0,0 +1,13 @@
+DEFAULT_NAME="cuda-cudnn-with-prereqs"
+
+DOCKER_REPO="wpafbo79/${DEFAULT_NAME}"
+
+CUDA_VERSION=$(grep "ENV CUDA_VERSION" Dockerfile | cut -d '=' -f 2)
+CUDNN_VERSION=$(grep "ENV CUDNN_VERSION" Dockerfile | cut -d '=' -f 2)
+TINI_VERSION=$(grep "ENV TINI_VERSION" Dockerfile | cut -d '=' -f 2)
+
+VERSION="cuda-${CUDA_VERSION}"
+VERSION="${VERSION}-cudnn${CUDNN_VERSION}"
+VERSION="${VERSION}_tini-${TINI_VERSION}"
+
+CREATE_VERSION="${VERSION}"
